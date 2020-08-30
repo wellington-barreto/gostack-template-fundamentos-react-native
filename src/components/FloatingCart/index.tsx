a8375b7f3235aff1c11f86aabf6b073472671543
Wellington Barreto <wellingtonbrt@gmail.com>
2020-08-30 00:35:03 -0300

    Desafio Finalizado

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,20 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productsSubTotal = product.price * product.quantity;
+      return accumulator + productsSubTotal;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    const total = products.reduce((accumulator, product) => {
+      const productsQuantityTotal = product.quantity;
+      return accumulator + productsQuantityTotal;
+    }, 0);
+    return total;
   }, [products]);
 
   return (
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,23 +39,28 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productsSubTotal = product.price * product.quantity;
+      return accumulator + productsSubTotal;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    const total = products.reduce((accumulator, product) => {
+      const productsQuantityTotal = product.quantity;
+      return accumulator + productsQuantityTotal;
+    }, 0);
+    return total;
   }, [products]);
 
   return (
